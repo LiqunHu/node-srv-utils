26542ca0d5c5d14a58d90a71556714ab2509a4c0
LiqunHu <huliquns@126.com>
2023-07-10 17:07:55 +0800

    add file upload

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import redisClient from './lib/redisClient'
 import authority from './lib/authority'
 import alisms from './lib/alisms'
 import scheduleJob from './lib/scheduleJob'
+import fileUtil from './lib/fileUtil'
 
 function setLogger(appointLogger: any) {
   alisms.setLogger(appointLogger)
@@ -12,4 +13,4 @@ function setLogger(appointLogger: any) {
 export { SecureConfig } from './lib/authority'
 export { AlismsConfig } from './lib/alisms'
 export { scheduleConfig } from './lib/scheduleJob'
-export { setLogger, alisms, authority, redisClient, scheduleJob }
+export { setLogger, alisms, authority, redisClient, scheduleJob, fileUtil }
---
+++ src/lib/fileUtil.ts
@@ -0,0 +1,58 @@
+import fs from 'fs'
+import path from 'path'
+import multiparty from 'multiparty'
+import { Request } from 'express'
+import mime from 'mime-types'
+
+let logger = console
+
+function setLogger(createLogger: any) {
+  logger = createLogger(__filename)
+}
+
+async function fileSaveLocal(req: Request, svpath: string, urlbase: string) {
+  return new Promise((resolve, reject) => {
+    if (req.is('multipart/*')) {
+      try {
+        if (!fs.existsSync(svpath)) {
+          let result = fs.mkdirSync(svpath, { recursive: true })
+          if (result) {
+            reject(result)
+          }
+        }
+        let uploadOptions = {
+          autoFields: true,
+          autoFiles: true,
+          uploadDir: svpath,
+          maxFileSize: 30 * 1024 * 1024,
+        }
+        let form = new multiparty.Form(uploadOptions)
+        form.parse(req, (err, fields, files) => {
+          if (err) {
+            reject(err)
+          }
+          if (files.file) {
+            logger.debug(files.file[0].path)
+            resolve({
+              name: files.file[0].originalFilename,
+              ext: path.extname(files.file[0].path),
+              url: urlbase + path.basename(files.file[0].path),
+              type: mime.lookup(path.extname(files.file[0].path)),
+              path: files.file[0].path,
+            })
+          } else {
+            reject('no file')
+          }
+        })
+      } catch (error) {
+        reject(error)
+      }
+    } else {
+      reject('content-type error')
+    }
+  })
+}
+
+export default {
+  fileSaveLocal,
+}
